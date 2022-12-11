815889545f7eac36c60505969b8c0ba583412b78
Martin Bångens <marbangens@gmail.com>
2022-12-11 17:30:18 +0100

    fix print

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@
  */
 
 #include <iostream>
+#include <cstdlib>
 #include <cmath>
 #include <cassert>
 #include <unistd.h>
@@ -34,14 +35,14 @@
 /********************************************************************
  * printHead - prints a headline
  */
-void printHead(const char* headline, int size, int col)
+void printHead(const char* headline, size_t size, int col)
 {
-    for (int i=0; i < col/2-size/2; i++)
+    for (int i = 0; i < col/2-size/2; i++)
       std::cout << "_";
 
     std::cout << headline;
 
-    for (int i=0; i < col/2-size/2; i++)
+    for (int i = 0; i < col/2-size/2; i++)
       std::cout << "_";
     std::cout << "\n";
 }
@@ -49,16 +50,34 @@ void printHead(const char* headline, int size, int col)
 /********************************************************************
  * printBuffer - prints the wave curve to the terminal with stars
  *
- * (windowSize/2)×(−1.0+1) = Most to the left
- * (windowSize/2)×(0.0 +1) = Center
- * (windowSize/2)×(1.0 +1) = Most to the right of the console (Linux)
+ * −1.0 = Most to the left
+ *  0.0 = Center
+ *  1.0 = Most to the right of the console (Linux)
+ *
+ * Variables
+ *
+ * size = buffer size
+ * col = terminal columns
+ * scale = print the curve scaled up to window vertical size (or frame buffer terminal)
  */
-void printBuffer(const float buff[], unsigned int size, int col, int scale = 1)
+void printBuffer(const float buff[], size_t size, int col, bool scale = 0)
 {
-  for(unsigned int i=0; i<size;i++)
+  float scaler = 1;
+  col = col - col%2;
+  if (scale)
   {
-    int spaces = floor((col/2)*(scale*buff[i]+1));
-    for (int j = 0; j<spaces; j++)
+    float max = fabs(buff[0]);
+    for (size_t i = 1; i < size; i++)
+      if (fabs(buff[i]) > max)
+        max = fabs(buff[i]);
+    
+    scaler = (1.0 / max)*0.995;
+  }
+
+  for(unsigned int i = 0; i < size; i++)
+  {
+    int spaces = ceil((col/2)*(scaler*buff[i]+1)) - 1;
+    for (int j = 0; j < spaces; j++)
       std::cout << ' ';
 
     std::cout << "*\n";
@@ -80,10 +99,12 @@ void printBuffer(const float buff[], unsigned int size, int col, int scale = 1)
  * col  = teminal size
  *
  */
-std::vector<float> getKernel(int sr, int col, int cutoff = 200, bool print = 1)
+std::vector<float> getKernel(const int sr, int col, int cutoff = 200, bool print = 1)
 {
   const size_t fftSize = 1024; // Needs to be power of 2!
-  const float binF = (sr/2)/(fftSize/2);
+  const float binF = (sr/2.0)/(fftSize/2.0);
+  if (cutoff > sr/2)
+    cutoff = sr/2;
 
   std::vector<float> re(audiofft::AudioFFT::ComplexSize(fftSize));
   std::vector<float> im(audiofft::AudioFFT::ComplexSize(fftSize));
@@ -93,10 +114,10 @@ std::vector<float> getKernel(int sr, int col, int cutoff = 200, bool print = 1)
   //                                 The Filter
   //******************************************************************************
   int   ci(cutoff/binF);
-  float cf(cutoff/binF-ci);
+  float cf(cutoff/binF - ci);
 
   //This is a brick wall low pass sinc kernel, have fun 
-  for (size_t i=0; i<(fftSize/2+1); i++)
+  for (size_t i = 0; i < fftSize/2+1; i++)
   {
     if (i <= ci)
       re[i] = 1;
@@ -106,7 +127,7 @@ std::vector<float> getKernel(int sr, int col, int cutoff = 200, bool print = 1)
       re[i] = 0;
   }
 
-  for (size_t i=0; i<(fftSize/2+1); i++)
+  for (size_t i = 0; i < fftSize/2+1; i++)
   {
     im[i] = sin(-i+ci+cf)/(-i+ci+cf);
   }
@@ -120,11 +141,11 @@ std::vector<float> getKernel(int sr, int col, int cutoff = 200, bool print = 1)
   if (print)
   {
     printHead("Kernel", 6, col);
-    printBuffer(output.data(), 512, col, 95 ); //scaled
+    printBuffer(output.data(), fftSize/2+1, col, true ); //scaled
     printHead("Real", 4, col);
-    printBuffer(re.data(), 512, col);
+    printBuffer(re.data(), fftSize/2+1, col);
     printHead("Imaginary_", 10, col);
-    printBuffer(im.data(), 512, col);
+    printBuffer(im.data(), fftSize/2+1, col);
   }
   return output;
 }
@@ -138,55 +159,66 @@ std::vector<float> getKernel(int sr, int col, int cutoff = 200, bool print = 1)
  */
 int main(int argc, char **argv)
 {
-  if (argc > 1)
+  if (argc > 2)
   {
-    std::cerr << "specify only one wav file (TODO)" << std::endl;
-  }	
+    std::cerr << "specify only one positive integer cutoff" << std::endl;
+  }
+
+  int cutoff = 200;
+  if (argc == 2)
+  {
+    cutoff = atoi(argv[1]);
+    if (cutoff < 0 )
+      std::cerr << "specify only positive integer cutoff" << std::endl;
+  }
+
   struct winsize wSize;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &wSize);
-  int col = wSize.ws_col;
 
   AudioFile<float> audioFile; 
   audioFile.load ("in.wav");
 
   int sampleRate = audioFile.getSampleRate();
-  int bitDepth = audioFile.getBitDepth();
+  //int bitDepth = audioFile.getBitDepth();
 
   int numSamples = audioFile.getNumSamplesPerChannel();
-  float lengthInSeconds = audioFile.getLengthInSeconds();
-  std::vector<float> kernel = getKernel(sampleRate, col);
+  //float lengthInSeconds = audioFile.getLengthInSeconds();
+  std::vector<float> kernel = getKernel(sampleRate, wSize.ws_col, cutoff);
 
-  int fftCalls = numSamples/1024;
-  int fftLastCall = numSamples%1024;
+  const int convolverSize = 1024;
+  const int kernelSize = kernel.size()/2 + 1;
+  const int fftCalls = numSamples / 1024;
+  const int fftLastCall = numSamples % 1024;
 
   if (audioFile.isMono())
   {
     fftconvolver::FFTConvolver Convolver;
-    assert (Convolver.init(1024, &kernel[0], 512));
-    float in[1024];
-    float out[1024];
+    assert (Convolver.init(convolverSize, kernel.data(), kernelSize));
+    float in[convolverSize];
+    float out[convolverSize];
+
     for (int i = 0; i < fftCalls; i++)
     {
-      for (int j = 0; j < 1024; j++)
+      for (int j = 0; j < convolverSize; j++)
       {
-	      in[j] = audioFile.samples[0][j+(i*1024)];
+	      in[j] = audioFile.samples[0][j + i*convolverSize];
       }
-      Convolver.process(&in[0], &audioFile.samples[0][i*1024], 1024);
+      Convolver.process(in, &audioFile.samples[0][i*convolverSize], convolverSize);
     }
     if (fftLastCall)
     {
       for (int i = 0; i < fftLastCall; i++)
       {
-	      in[i] = audioFile.samples[0][i+(fftCalls*1024)];
+	      in[i] = audioFile.samples[0][i+(fftCalls*convolverSize)];
       }
-      for (int i = fftLastCall; i < 1024; i++)
+      for (int i = fftLastCall; i < convolverSize; i++)
       {
 	      in[i] = 0;
       }
-      Convolver.process(&in[0], &out[0], 1024);
+      Convolver.process(in, out, convolverSize);
       for (int i = 0; i < fftLastCall; i++)
       {
-	      audioFile.samples[0][i+(fftCalls*1024)] = out[i];
+	      audioFile.samples[0][i + fftCalls*convolverSize] = out[i];
       }
     }
   }
@@ -194,40 +226,41 @@ int main(int argc, char **argv)
   {
     fftconvolver::FFTConvolver Convolver1;
     fftconvolver::FFTConvolver Convolver2;
-    assert (Convolver1.init(1024, &kernel[0], 512));
-    assert (Convolver2.init(1024, &kernel[0], 512));
-    float in1[1024];
-    float in2[1024];
-    float out1[1024];
-    float out2[1024];
+    assert (Convolver1.init(convolverSize, kernel.data(), kernelSize));
+    assert (Convolver2.init(convolverSize, kernel.data(), kernelSize));
+    float in1[convolverSize];
+    float in2[convolverSize];
+    float out1[convolverSize];
+    float out2[convolverSize];
+
     for (int i = 0; i < fftCalls; i++)
     {
-      for (int j = 0; j < 1024; j++)
+      for (int j = 0; j < convolverSize; j++)
       {
-	      in1[j] = audioFile.samples[0][j+(i*1024)];
-	      in2[j] = audioFile.samples[1][j+(i*1024)];
+	      in1[j] = audioFile.samples[0][j + i*convolverSize];
+	      in2[j] = audioFile.samples[1][j + i*convolverSize];
       }
-      Convolver1.process(&in1[0], &audioFile.samples[0][i*1024], 1024);
-      Convolver2.process(&in2[0], &audioFile.samples[1][i*1024], 1024);
+      Convolver1.process(in1, &audioFile.samples[0][i*convolverSize], convolverSize);
+      Convolver2.process(in2, &audioFile.samples[1][i*convolverSize], convolverSize);
     }
     if (fftLastCall)
     {
       for (int i = 0; i < fftLastCall; i++)
       {
-	      in1[i] = audioFile.samples[0][i+(fftCalls*1024)];
-	      in2[i] = audioFile.samples[1][i+(fftCalls*1024)];
+	      in1[i] = audioFile.samples[0][i + fftCalls*convolverSize];
+	      in2[i] = audioFile.samples[1][i + fftCalls*convolverSize];
       }
-      for (int i = fftLastCall; i < 1024; i++)
+      for (int i = fftLastCall; i < convolverSize; i++)
       {
 	      in1[i] = 0;
 	      in2[i] = 0;
       }
-      Convolver1.process(&in1[0],&audioFile.samples[0][fftCalls*1024],1024);
-      Convolver2.process(&in2[0], &out2[0], 1024);
+      Convolver1.process(in1, out1, convolverSize);
+      Convolver2.process(in2, out2, convolverSize);
       for (int i = 0; i < fftLastCall; i++)
       {
-	      audioFile.samples[0][i+(fftCalls*1024)] = out1[i];
-	      audioFile.samples[1][i+(fftCalls*1024)] = out2[i];
+	      audioFile.samples[0][i + fftCalls*convolverSize] = out1[i];
+	      audioFile.samples[1][i + fftCalls*convolverSize] = out2[i];
       }
     }
   }
